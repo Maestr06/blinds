bb9b455d2f3eefde07c7bcb3198c68373c2ac784
Marceli Koćwin <988marceli@gmail.com>
2021-04-25 19:08:03 +0200

    dodano podstawowoą automatyzacje

--- projekt/projekt.ino
+++ projekt/projekt.ino
@@ -5,33 +5,43 @@
 #include <BH1750.h>
 #define I2C_ADDRESS 0x76
 
-
-
+bool closed = true;
+bool temp_closed = false;
+const char left = 'l';
+const char right = 'r';
 int stepCount = 0;  // number of steps the motor has taken
-const int dirpin = 4;
+const int dirpin = 4; //HIGH is left rotation, LOW is right rotation
 const int steppin = 3;
-float temperature;
+float temperature = 20;
 float lux;
 const int MS1=8;
 const int MS2=9;
 const int MS3=10;
+const int speedofmotor = 60;
+const unsigned int rotation = 6400; // steps for one rotation on 1/32 microstep mode
 
 BMx280TwoWire bmx280(&Wire, I2C_ADDRESS);
 BH1750 lightMeter;
-void light_read(); //checking current lux value
-void temp_read(); //checking current temp
-void left();
-void right();
+void light_read(); // checking current lux value
+void temp_read(); // checking current temp
+void turn_left();
+void turn_right();
+void rotate(char rotate_dir, int rotations);
 
 
 void setup() {
+  
   Serial.begin(9600);
   pinMode(dirpin, OUTPUT);
   pinMode(steppin, OUTPUT);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
   pinMode(MS1, OUTPUT);
-  digitalWrite(MS1, HIGH)
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
   while (!Serial);
   Wire.begin();
   //begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
@@ -51,12 +61,34 @@ void setup() {
   lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
 
   Serial.println(F("BH1750 One-Time Test"));
+  
 }
 
-void loop() {  
+void loop() { 
   temp_read();
-  light_read(); 
-
+  light_read();
+  turn_right();
+  turn_left();
+  if (lux >= 200 && closed == true && temp_closed == false) {
+    rotate('l', 7);
+    closed = false;
+  }
+  else if (lux < 200 && closed == false && temp_closed == false) {
+    rotate('r', 7);
+    closed = true;
+  }
+  else if (lux < 200 && closed == false && temp_closed == true) {
+    rotate('r', 10);
+    closed = false;
+  }
+  else if (temperature >= 26 && closed == false && temp_closed == false) {
+    rotate('l', 3);
+    temp_closed = true;
+  }
+  else if (temperature < 26 && closed == false && temp_closed == true) {
+    rotate('r', 3);
+    temp_closed = false;
+  }
 }
 
 void light_read() {
@@ -64,7 +96,7 @@ void light_read() {
   while (!lightMeter.measurementReady(true)) {
     yield();
   }
-  float lux = lightMeter.readLightLevel();
+  lux = lightMeter.readLightLevel();
   Serial.print("Light: "); Serial.print(lux); Serial.println(" lx");
 }
 
@@ -81,24 +113,41 @@ if (!bmx280.measure())
 
   Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
   temperature = bmx280.getTemperature();
-}  
+}
 
-void right() {
-  digitalWrite(dirpin, LOW);
-  while(digitalRead(5) == LOW) {
+void rotate(char rotate_dir, int rotations) {
+  if (rotate_dir == left) {
+    digitalWrite(dirpin, HIGH);
+  }
+  else if (rotate_dir == right) {
+    digitalWrite(dirpin, LOW);
+  }
+  Serial.println(rotation*rotations);
+  for (int i = 0; i < (rotation*rotations); i++) {
+    
     digitalWrite(steppin,HIGH); 
-    delayMicroseconds(1000); 
+    delayMicroseconds(speedofmotor); 
     digitalWrite(steppin,LOW); 
-    delayMicroseconds(1000);
+    delayMicroseconds(speedofmotor);
   }
 }
 
-void left() {
+void turn_right() {
+  digitalWrite(dirpin, LOW);
+  while(digitalRead(5) == LOW) {
+    digitalWrite(steppin,HIGH); 
+    delayMicroseconds(speedofmotor); 
+    digitalWrite(steppin,LOW); 
+    delayMicroseconds(speedofmotor);
+  }
+}
+
+void turn_left() {
   digitalWrite(dirpin, HIGH);
   while(digitalRead(6) == LOW) {
     digitalWrite(steppin,HIGH); 
-    delayMicroseconds(1000); 
+    delayMicroseconds(speedofmotor); 
     digitalWrite(steppin,LOW); 
-    delayMicroseconds(1000);
+    delayMicroseconds(speedofmotor);
   }
 }
